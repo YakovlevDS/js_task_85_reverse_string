df3354693a2136b95bc6ea9727e0384bf4d385c3
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-09-23 20:32:32 +0200

    added reverse str

---
+++ app.js
@@ -0,0 +1,10 @@
+// ? Task: Reverse a string
+
+// Solution 1
+const reverse = str => str.split('').reverse().join('');
+
+console.log(reverse("hello world"));   
+// Result: 'dlrow olleh'
+
+
+// ! Explanation: You can easily reverse a string using split, reverse and join methods.
